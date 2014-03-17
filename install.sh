a1acd772f93705c828230d06e8e01647127412d7
Mike Holm <mholm815@gmail.com>
2014-03-16 23:08:18 -0500

    make dest easier to modify

--- install.sh
+++ install.sh
@@ -4,9 +4,10 @@ PRGDIR=`dirname "$0"`
 
 pushd $PRGDIR/src > /dev/null
 _SRCDIR=`pwd`
+_DESTDIR=$HOME
 for f in *; do
     _SRC=$_SRCDIR/$f
-    _DST=$HOME/.$f
+    _DST=$_DESTDIR/.$f
     rm -ri $_DST
     ln -s $_SRC $_DST
 done
